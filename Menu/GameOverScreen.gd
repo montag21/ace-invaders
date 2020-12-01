ff7fbcca84599d60c7b4c9202a0517bdfff60825
Oleksii Sosevych <oleksiisosevych@gmail.com>
2020-12-01 21:13:10 +0000

    fix GameOverScreen

--- Menu/GameOverScreen.gd
+++ Menu/GameOverScreen.gd
@@ -3,3 +3,15 @@ extends Control
 
 func _ready():
 	$"Menu/CenterRow/Buttons/Game Over".text = "BIG WIN!\nYOUR FINAL SCORE IS:\n%d" % ScoreManager.score
+
+
+func _on_PlayButton_pressed():
+	get_tree().change_scene("res://Menu/Intro/GeneralSpeechFirst.tscn")
+
+
+func _on_CreditsButton_pressed():
+	get_tree().change_scene("res://Menu/CreditsScreen.tscn")
+
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
